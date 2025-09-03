35b0e9fd542779bcf2492194725d9a6e721bcfda
Alessandro Saracino <Drosan37@users.noreply.github.com>
2025-09-03 13:53:02 +0200

    Update IN_Chk_Backups.sql
    Update with new query for retrieve last n backup for every types

--- IN_Chk_Backups.sql
+++ IN_Chk_Backups.sql
@@ -1,21 +1,37 @@
--- Check backup status
-SELECT 
-	  msdb.dbo.backupset.database_name AS 'BackupName'
-	, msdb.dbo.backupset.backup_start_date AS 'StartDate'
-	, msdb.dbo.backupset.backup_finish_date AS 'StopDate'
-	, msdb.dbo.backupset.expiration_date AS 'ExpirationDate'
-	, CASE msdb..backupset.type 
-		WHEN 'D' THEN 'Database' 
-		WHEN 'L' THEN 'Log' 
-	END AS 'BackupType'
-	, CAST(msdb.dbo.backupset.backup_size/1024/1024 AS NUMERIC(18,2)) AS 'BackupSize (MB)'
-	, msdb.dbo.backupmediafamily.logical_device_name AS 'LogicalDeviceName'
-	--, msdb.dbo.backupmediafamily.physical_device_name 
-	, msdb.dbo.backupset.name AS 'BackupsetName'
-	--, msdb.dbo.backupset.description 
-FROM msdb.dbo.backupmediafamily 
-	INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
-WHERE (CONVERT(datetime, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 7) 
-ORDER BY 
-	  msdb.dbo.backupset.database_name 
-	, msdb.dbo.backupset.backup_finish_date 
+-- Check last backup for DB
+DECLARE 
+	  @RetentionDays INT = 7
+	, @RowNumber INT = 1
+BEGIN
+	SELECT
+		  BackupName
+		, StartDate
+		, StopDate
+		, BackupType
+		, [BackupSize (MB)]
+		, BackupsetName
+	FROM 
+	(
+		SELECT DISTINCT
+			  msdb.dbo.backupset.database_name AS 'BackupName'
+			, msdb.dbo.backupset.backup_start_date AS 'StartDate'
+			, msdb.dbo.backupset.backup_finish_date AS 'StopDate'
+			, msdb.dbo.backupset.expiration_date AS 'ExpirationDate'
+			, CASE msdb.dbo.backupset.type 
+				WHEN 'D' THEN 'Database' 
+				WHEN 'L' THEN 'Log' 
+			END AS 'BackupType'
+			, CAST(msdb.dbo.backupset.backup_size/1024/1024 AS NUMERIC(18,2)) AS 'BackupSize (MB)'
+			, msdb.dbo.backupmediafamily.logical_device_name AS 'LogicalDeviceName'		
+			, msdb.dbo.backupset.name AS 'BackupsetName'		
+			, ROW_NUMBER() OVER (PARTITION BY msdb.dbo.backupset.database_name, msdb..backupset.type  ORDER BY msdb.dbo.backupset.backup_finish_date DESC) AS RowNum
+		FROM msdb.dbo.backupmediafamily 
+			INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 	
+	) tbl
+	WHERE 1=1
+	AND (StartDate >= GETDATE() - @RetentionDays)
+	AND RowNum <= @RowNumber
+	ORDER BY 
+		  BackupName 
+		, StopDate DESC
+END
